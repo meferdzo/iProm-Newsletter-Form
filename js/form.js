0f623f5a0200b6ffb4a53bccb0e84abc94f9c978
meferdzo <rok.ferjancic.20@gmail.com>
2018-06-13 21:53:53 +0200

    Finished homework.

---
+++ js/form.js
@@ -0,0 +1,34 @@
+console.log("I'm connected");
+
+var closeBtn = document.getElementById("close");
+var form = document.getElementById("main-wrapper");
+
+form.style.opacity = 1;
+form.style.transition = "opacity .5s";
+
+const fade = (() => {
+  form.style.opacity = 0;
+});
+
+closeBtn.addEventListener("click", fade, false)
+
+function validateform(){
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+
+  if(name == "" || email == ""){
+    console.log("empty");
+    document.getElementById('name-error').innerHTML = 'Izpolnite to polje.'
+    document.getElementById('email-error').innerHTML = 'Izpolnite to polje.'
+    return false;
+
+  } else if (name == "Ime_in_priimek" || email == "Elektronski_naslov"){
+    console.log("empty halo");
+    document.getElementById('name-error').innerHTML = 'Izpolnite to polje.'
+    document.getElementById('email-error').innerHTML = 'Izpolnite to polje.'
+    return false;
+  }  else{
+    console.log("thanks")
+    return true;
+  }
+}
